de798e5f7ff8a5cb03f1e7644256aca4fd17b2b0
ChiaraBuono <7687956@studenti.unige.it>
2024-11-12 07:13:22 +0000

    Test watchdog

--- watchdog.c
+++ watchdog.c
@@ -8,14 +8,60 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <signal.h>
+
+int period = 10;        // period after which the check is done
+
+// define process monitored variables
+#define MAX_PROCESSES 3
+pid_t monitored_pids[MAX_PROCESSES];
+
+void handler(int signal_number) {
+    period--;
+    printf("%d", period);
+    fflush(stdout);
+    if (period == 0) {
+        for (int i = 0; i < MAX_PROCESSES; i++)
+            printf("Checking if process still alive %d", monitored_pids[i]);
+            fflush(stdout);
+            /* if (check if it is running ){
+
+            } else {
+
+            } */
+        period = 10;
+    }
+
+}
 
 int main(int argc, char *argv[])
 {
-    pid_t pid = getpid();
-    while (1)
-    {
-        printf( "watchdog: %d\n", pid);
-        sleep(5);
+    printf( "watchdog: %d\n", getpid());
+    char *pids_str = getenv("MONITORED_PIDS");
+
+    if (pids_str != NULL) {
+
+        char *token = strtok(pids_str, ",");
+
+        /*
+        while (token != NULL) {
+
+            monitored_pids[num_monitored_processes++] = token;
+
+            token = strtok(NULL, ",");
+
+        }
+        */
+
+    }
+    
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = &handler;
+
+    while(1) {
+        sigaction (SIGUSR1, &sa, NULL);
+        sleep(1);
     }
     
     return 0;
